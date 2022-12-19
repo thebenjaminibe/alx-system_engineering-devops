165c50ea2ef32893d29341d6fb500caefce1fea6
Benjamin Ibe <97017874+thebenjaminibe@users.noreply.github.com>
2022-12-19 16:09:47 +0100

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,6 +0,0 @@
-# Fixing stack
-exec { 'file limit':
-  onlyif   => 'test -e /etc/default/nginx',
-  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
-}
